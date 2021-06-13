5ed309b866094b39bed08f65ff9b8da04ebd8c97
Anubhav3084 <58735455+Anubhav3084@users.noreply.github.com>
2021-06-13 16:46:51 +0530

    Add files via upload

---
+++ Rotary_encoder.v
@@ -0,0 +1,109 @@
+`timescale 1ns / 1ps
+
+/*------------------------------------------------------------------------------------
+* This is the implemetation of rotary encoder.
+* This module takes input pulses from the motor and give a 7-bit output.
+* The output is used to determine the speed of the motor.
+* Implemented using FSM. 
+* In state Increment_enc we detect whether there is a pulse or not and then 
+    if there is a pulse then we increment the temp_enc register
+* In latch state we send the data.
+* The prescalar value is calculated such that that at max speed we will get 
+   output as 127
+--------------------------------------------------------------------------------------*/
+
+module Rotary_encoder(clk, pulse, reset, enc, temp_pulse);
+	//inputs 
+	input clk;
+	input pulse;
+	input reset;
+	
+	// outputs
+	output reg [6:0]enc = 7'd0;	// output encoder data
+	output reg temp_pulse = 1'b0;	// output pulse indicating the state
+
+	reg [1:0] state = 2'b0;			// variable to store present state
+	reg [1:0] next_state = 2'b0;	// variable to store next state
+	reg [1:0] data = 2'b0;			// to detect pulse data received from motor
+
+	reg [32:0]count = 32'd0;		// counter used for determining the duration of any particular state
+	
+	reg [6:0]temp_enc = 7'd0; 		// temporary storage for data calculation
+
+	parameter IDLE = 2'b00, Increment_enc = 2'b01, Latch = 2'b10;	// state parameters
+	
+	parameter prescaler = 60000;	// prescalar used for determining the duration of any particular state
+//------------------------------------------------------------------------------------------------------------------------------
+	// This always block generates temp_pulse 
+	// temp_pulse is low for two clock cycle where we send data
+	// temp_pulse is high for some clock cycles which is decided by prescalar for getting data for calculating speed
+	always @(posedge clk)
+	begin
+		// low for two clock cycle
+		if(count == (prescaler-1'b1)) 
+		begin
+			count <= count+1;
+			temp_pulse <= 0;
+		end
+		
+		else if(count == prescalar)
+		begin
+			count <=0;
+			temp_pulse <= 0;
+		end
+		
+		else
+		begin
+			count <= count+1;
+			temp_pulse <= 1; 
+		end
+	end
+//------------------------------------------------------------------------------------------------------------------------------
+
+	// FSM implementation
+	always @(posedge clk)
+	 begin 
+		 if (reset == 1'b1) state <=   IDLE;
+		 else state <=  next_state;
+	 end
+//------------------------------------------------------------------------------------------------------------------------------
+
+	// This module determines the next state of our FSM
+	always @(temp_pulse)
+	begin
+		case(state)
+		
+			IDLE : if (temp_pulse == 1'b1) next_state = Increment_enc;
+					  else next_state = IDLE;
+			  
+			Increment_enc : if (temp_pulse == 1'b1) next_state = Increment_enc;
+									else next_state = Latch;
+			  
+			Latch : if(temp_pulse == 1) next_state = Increment_enc;
+						else next_state = IDLE;
+			  
+			default : next_state = IDLE;
+		  
+		 endcase
+	end
+//------------------------------------------------------------------------------------------------------------------------------
+
+	// This module detects if a pulse comes from motor and stores it
+	always @(posedge clk)
+	begin
+	
+		data[1] <= data[0]; // This is a shift register. We store previous pulse in data[1] and new pulse in data[0]
+		data[0] <= pulse;
+	
+		case(state)
+			IDLE : temp_enc <= 0;
+			
+			Increment_enc : temp_enc <= temp_enc + (data[0]^data[1]);	// Here increment if a pulse comes. Here temp_enc give [0,128]
+			
+			Latch : if(temp_enc == 0) enc <= 0; else enc <= temp_enc-1;		// Here we send data. We send data from [0,127]
+			
+			default : temp_enc <= 0;
+		endcase
+	end
+	
+endmodule
---
+++ dribbler.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+
+/*---------------------------------------------------------------------------------------
+* This is the implementation of dribbler module
+* Dribbler is controlled by a BLDC motor so it will be same as the motorcontrol module
+* The difference between this and motormodule is that duty cycle for PWM is 100 %
+* We take data from HALL sensors and based on the commutation table generate the output 
+   mosfets of different phases of motor
+---------------------------------------------------------------------------------------*/
+
+module dribbler(enable ,Hall, a ,b ,c);
+
+	input [2:0]Hall;	// inputs from hall sensors
+	input enable;		// for enabling the dribbler
+
+	output [1:0]a;		// output for inputs of the mosfets of phase A
+	output [1:0]b;		// output for inputs of the mosfets of phase B
+	output [1:0]c;		// output for inputs of the mosfets of phase C
+
+	reg dir = 1;		// dribbler direction of rotation is constant.
+	
+	reg [1:0]x;			// temporary register for calculation of the output for phase A
+	reg [1:0]y;			// temporary register for calculation of the output for phase B
+	reg [1:0]z;			// temporary register for calculation of the output for phase C
+
+	// wires for hall sensors. Not necessarily needed but used so that code looks simple
+	wire H1 = Hall[0];	
+	wire H2 = Hall[1];
+	wire H3 = Hall[2];
+//------------------------------------------------------------------------------------------------------------------------------
+
+	// whenever hall sensors value is changed or enable is changed, this always block gets activated
+	always@(Hall or enable) 
+	begin
+	
+		// if enable is high then we run the dribbler
+		if(enable) 
+			begin
+				// this condition can't be achieved in normal running.
+				// in case it occurs then motor is at float condition.
+				if(Hall == 7 || Hall == 0)
+				begin
+					x = 2'b01;
+					y = 2'b01;
+					z = 2'b01;
+				end
+				// here we calculate how to switch phases depending on the inputs of hall sensor.
+				// direction is fixed to 1
+				else 
+					begin
+					x[0] = ((~dir&~H2)|(H1&H2)|(dir&~H1));
+					x[1] = ((~dir&H1&~H2)|(dir&~H1&H2));
+					y[0] = ((~dir&~H3)|(H2&H3)|(dir&~H2));
+					y[1] = ((~dir&H2&~H3)|(dir&~H2&H3));
+					z[0] = ((~dir&~H1)|(H1&H3)|(dir&~H3));
+					z[1] = ((~dir&~H1&H3)|(dir&H1&~H3));
+				end
+			end
+			
+		// if enable is not high then we set the motor to float condition
+		else 
+			begin
+				x = 2'b01;
+				y = 2'b01;
+				z = 2'b01;
+			end
+	end
+//------------------------------------------------------------------------------------------------------------------------------
+
+	// assignment of temporary variables to output
+	assign a = x;
+	assign b = y;
+	assign c = z;
+
+endmodule
